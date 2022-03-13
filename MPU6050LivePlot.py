c5415f4fadc70c39c2dc2aa10838a10c9fcac180
Radek Pudelko <rmpudelk@ncsu.edu>
2022-03-12 19:38:19 -0500

    live read of all accel channels

--- MPU6050LivePlot.py
+++ MPU6050LivePlot.py
@@ -6,87 +6,97 @@ import MPU6050
 import MPU6050Buffer
 
 def shiftAndInsert(arr, val):
-    arr[0:len(arr) - len(val)] = arr[len(val):]
-    arr[len(arr) - len(val):] = val
+    if len(val) < len(arr):
+        arr[0:len(arr) - len(val)] = arr[len(val):]
+        arr[len(arr) - len(val):] = val
+    else:
+        return val[len(val)-len(arr):]
     return arr
-    #arr = np.delete(arr, 0)
-    #return np.append(arr, val)
+
 
 
 def main():
     sampleRate = 25
-    timeToDisplay = 4
-    x = np.linspace(timeToDisplay, 0, num=100) # Preallocate 100 size array
+    timeToDisplay = 8
+    numPoints = sampleRate*timeToDisplay
+    x = np.linspace(timeToDisplay, 0, num=numPoints) # Preallocate 100 size array
     fig = plt.figure()
-    ax1 = fig.add_subplot(1, 1, 1)
-    y = np.zeros(100)
+    ax1 = fig.add_subplot(3, 1, 1)
+    ax2 = fig.add_subplot(3, 1, 2)
+    ax3 = fig.add_subplot(3, 1, 3)
+    accelX = np.zeros(numPoints)
+    accelY = np.zeros(numPoints)
+    accelZ = np.zeros(numPoints)
 
-    line, = ax1.plot([], lw=3)
-    text = ax1.text(1,1.6, "")
+    line1, = ax1.plot([], lw=3)
+    line2, = ax2.plot([], lw=3)
+    line3, = ax3.plot([], lw=3)
+    text = ax1.text(3.8,.75, "")
 
     # Set x and y axis limits
     ax1.set_xlim(x.max(), x.min())
     ax1.set_ylim([-2, 2])
+    ax2.set_xlim(x.max(), x.min())
+    ax2.set_ylim([-2, 2])
+    ax3.set_xlim(x.max(), x.min())
+    ax3.set_ylim([-2, 2])
 
     fig.canvas.draw()   # note that the first draw comes before setting data 
 
-    # cache the background
+    # Cache backgrounds
     ax1background = fig.canvas.copy_from_bbox(ax1.bbox)
-
+    ax2background = fig.canvas.copy_from_bbox(ax2.bbox)
+    ax3background = fig.canvas.copy_from_bbox(ax3.bbox)
     plt.show(block=False)
 
-    t_start = time.time()
-
-    line.set_data(x, y) # sine wave update
-    #line.set_data(x, np.sin(x/3.+k)) # sine wave update
-
-    # restore background
-    fig.canvas.restore_region(ax1background)
-
-    # redraw just the points
-    ax1.draw_artist(line)
-    ax1.draw_artist(text)
-
-    # fill in the axes rectangle
-    fig.canvas.blit(ax1.bbox)
-
-    fig.canvas.flush_events()
-
-    # Basically, we recompute the full sinewave each time, but we increment k whic effectivly adds
-    # one point to the plot
+    # Start the sensor and buffer
     sensor = MPU6050.MPU6050()
     buf = MPU6050Buffer.MPU6050Buffer(sensor)
     buf.start()
-    for i in np.arange(1000):
-        points = buf.bufferLen()
-        data = []
-        for j in range(points):
-            a = buf.read()[0]
-            data.append(a)
-        y = shiftAndInsert(y, data)
 
+    t_start = time.time()
+    # for i in np.arange(1000):
+    i=0
+    while True:
+        # Read the new points from the buffer
+        numPoints = buf.bufferLen()
+        newPoints = [[],[],[]]
+        for j in range(numPoints):
+            a = buf.read()
+            newPoints[0].append(a[0])
+            newPoints[1].append(a[1])
+            newPoints[2].append(a[2])
+        # Shift the old data points and insert the new ones
+        accelX = shiftAndInsert(accelX, newPoints[0])
+        accelY = shiftAndInsert(accelY, newPoints[1])
+        accelZ = shiftAndInsert(accelZ, newPoints[2])  
             
-        # a = sensor.readLive()[0]
-        # print(a)
-            #y = shiftAndInsert(y, a)
-
-        line.set_data(x, y) # sine wave update
-        #line.set_data(x, np.sin(x/3.+k)) # sine wave update
-        tx = 'Mean Frame Rate:\n {fps:.3f}FPS'.format(fps= ((i+1) / (time.time() - t_start)) ) 
+        line1.set_data(x, accelX)
+        line2.set_data(x, accelY)
+        line3.set_data(x, accelZ)
+        i+=1
+        tx = 'Mean Frame Rate:\n {fps:.3f}FPS'.format(fps= ((i) / (time.time() - t_start)) ) 
         text.set_text(tx)
 
         # restore background
         fig.canvas.restore_region(ax1background)
+        fig.canvas.restore_region(ax2background)
+        fig.canvas.restore_region(ax3background)
 
         # redraw just the points
-        ax1.draw_artist(line)
+        ax1.draw_artist(line1)
+        ax2.draw_artist(line2)
+        ax2.draw_artist(line3)
         ax1.draw_artist(text)
 
         # fill in the axes rectangle
         fig.canvas.blit(ax1.bbox)
+        fig.canvas.blit(ax2.bbox)
+        fig.canvas.blit(ax3.bbox)
 
+        # Update
         fig.canvas.flush_events()
-    time.sleep(10)
+    # time.sleep(10)
 
 
 

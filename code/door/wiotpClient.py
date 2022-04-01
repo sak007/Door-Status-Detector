6e2a2706211903c6154cf62d9b808603b0937284
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-04-01 10:36:56 -0400

    feat: publish event data to cloud

--- code/door/wiotpClient.py
+++ code/door/wiotpClient.py
@@ -2,14 +2,14 @@ import wiotp.sdk.device
 import json
 import uuid
 
-class Client:
+class DeviceClient:
 
     def __init__(self):
-        f = open('../properties.json')
+        f = open('../../properties.json')
         properties = json.load(f)
         self.typeId = properties['DEVICE_TYPE']
         self.deviceId = properties['DEVICE_ID']
-        options = wiotp.sdk.device.parseConfigFile("device.yaml")
+        options = wiotp.sdk.device.parseConfigFile("../device.yaml")
         self.client = wiotp.sdk.device.DeviceClient(config=options)
         self.client.connect()
 
@@ -18,3 +18,14 @@ class Client:
 
     def publishEventCallback(self):
         print ("Event data published!")
+
+if __name__ == "__main__":
+    try:
+        client = DeviceClient()
+        while True:
+            eventData = {'Test' : True}
+            client.publish('door', eventData)
+            sleep(5)
+
+    except Exception as e:
+        print("Exception: ", e)

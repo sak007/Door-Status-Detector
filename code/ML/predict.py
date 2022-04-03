90aa3c926eb19439dd8a6b5800b84e70f5a2fcfc
Radek <radek@MacBook-Pro.local>
2022-04-03 14:36:55 -0400

    added prediction to classifier client

---
+++ code/ML/predict.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+import numpy as np
+import os
+import copy
+
+MODEL_PATH = "code/ML/myModel"
+
+TestFolder = "data/jppahl/"
+
+MaxSamples = 600 # Num Gx Samples for Model
+
+
+def getClass(file):
+    c = (file[:-4].split("_")[1])
+    if c == "c":
+        return 0
+    elif c =="o":
+        return 1
+    else:
+        raise Exception("Unknown class")
+
+# Read the gx data from start:stop
+def readData(file):
+    data = []
+    with open(file, "r") as f:
+        lines = f.readlines()
+        start = int(lines[4].split(",")[1])
+        stop = int(lines[5].split(",")[1])
+        myclass = getClass(file)
+        gx = lines[9].split(",")
+        assert gx[0] == "gx"
+        for val in gx[start:stop]:
+            data.append(float(val))
+    return data, myclass
+
+def normalize(data):
+    mean = np.mean(data)
+    sd = np.std(data)
+    data2 = (data - mean) / sd
+    return list(data2)
+
+# Fills data list up until the length matches n
+def fill(data, n, val=0):
+    data2 = copy.deepcopy(data)
+    diff = n - len(data2)
+    for i in range(diff):
+        data2.append(0)
+    return data2
+
+# Normalizes data and puts it into the correct shape for the model
+def preprocessData(data):
+    data = normalize(data)
+    data = fill(data, MaxSamples)
+    return np.dstack([data])
+
+def predict(model, data):
+    predictRaw = model.predict(data)
+    return np.argmax(predictRaw)
+
+# This is to test out loading a saved model and having it predict against data
+# defined in the files in MyFolder. 
+def main():
+    myModel = tf.keras.models.load_model(MODEL_PATH)
+    myModel.summary()
+    files = os.listdir(TestFolder)
+    badClasses = 0
+    for file in files:
+        data, myclass = readData(TestFolder + file)
+        data = preprocessData(data)
+        predictedClass = predict(myModel, data)
+        if not myclass == predictedClass:
+            badClasses+=1
+            print(file)
+    print("Misclassifications: ", badClasses)
+
+if __name__ == "__main__":
+    main()
--- code/classifier/subscriber.py
+++ code/classifier/subscriber.py
@@ -1,17 +1,57 @@
 from wiotpApplicationClient import ApplicationClient
+import numpy as np
+import tensorflow as tf
+import copy
+
+MaxSamples = 600 # Num Gx Samples for Model
+MODEL_PATH = "myModel"
+
+def normalize(data):
+    mean = np.mean(data)
+    sd = np.std(data)
+    data2 = (data - mean) / sd
+    return list(data2)
+
+# Fills data list up until the length matches n
+def fill(data, n, val=0):
+    data2 = copy.deepcopy(data)
+    diff = n - len(data2)
+    for i in range(diff):
+        data2.append(0)
+    return data2
+
+# Normalizes data and puts it into the correct shape for the model
+def preprocessData(data):
+    data = normalize(data)
+    data = fill(data, MaxSamples)
+    return np.dstack([data])
+
+# Given the prepreocessed gx
+def predict(model, data):
+    result = np.argmax(model.predict(preprocessData(data)))
+    if result == 0:
+        return "close"
+    else:
+        return "open"
+
+# Load the nn model
+def initModel(path):
+    return tf.keras.models.load_model(path)
 
 class ClassifierAppClient(ApplicationClient):
+    def __init__(self):
+        super(ClassifierAppClient, self).__init__()
+        self.model = initModel(MODEL_PATH)
     def onMessage(self, event):
         if event.eventId == 'imu':
-            # TODO: Call respective classifier
             print(event.eventId, event.data)
-
+            status = predict(self.model, event.data["gx"])
             # Process data and send decision
             status = 'open'
             data = {"status": status}
             self.sendCommand('doorCommand', data)
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     try:
         client = ClassifierAppClient()
         client.client.subscribeToDeviceEvents(eventId="imu")
